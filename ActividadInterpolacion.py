b2149bad350ffb45ee694f62f5455c08dd7acfa1
SantiagoHurValen <santiagohurvalen@gmail.com>
2024-02-19 23:09:37 -0500

    Correcciones varias de gráficas y textos, además de conclusiones

--- ActividadInterpolacion.py
+++ ActividadInterpolacion.py
@@ -1,3 +1,5 @@
+##Actividad Interpolación
+#Por: Santiago Hurtado Valencia para Programación para Ingeniería - Maestría en Ingeniería Cohorte VIII
 #Planteamiento del problema:
 #Dentro del problema dado en la actividad se busca dar un optimización a un mecanismo de línea recta de cuatro barras, la idea es encontrar
 #las dimensiones óptimas para tener una línea recta y una velocidad constante sin mucho margen de error
@@ -5,7 +7,6 @@
 #Importación de librerías a usar dentro del código
 from scipy.interpolate import lagrange
 from scipy.interpolate import InterpolatedUnivariateSpline
-from scipy.interpolate import barycentric_interpolate
 import numpy as np 
 import matplotlib.pyplot as plt 
 
@@ -62,7 +63,7 @@ plt.grid(color='gray', linestyle='dashed', linewidth=1, alpha=0.4)
 plt.subplot(1,3,2)
 plt.plot(t,f2,color='red', label='Interpolacion Lagrange')
 plt.plot(x1,y2,':x',mew=2,color='royalblue',label='Datos originales')
-plt.legend(loc='upper left')
+plt.legend(loc='upper right')
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("Gráfica para los datos L3/L2 en rectitud")
@@ -71,7 +72,7 @@ plt.grid(color='gray', linestyle='dashed', linewidth=1, alpha=0.4)
 plt.subplot(1,3,3)
 plt.plot(t,f3, color='red', label='Interpolacion Lagrange')
 plt.plot(x1,y3,':x',mew=2, color='royalblue',label='Datos originales')
-plt.legend(loc='upper left')
+plt.legend(loc='upper right')
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("Gráfica para los datos L1/L2 en rectitud")
@@ -93,7 +94,7 @@ plt.grid(color='gray', linestyle='dashed', linewidth=1, alpha=0.4)
 plt.subplot(1,3,2)
 plt.plot(t,f5, color='red', label='Interpolacion Lagrange')
 plt.plot(x1,v2,':x',mew=2, color='royalblue',label='Datos originales')
-plt.legend(loc='upper left')
+plt.legend(loc='upper right')
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("Gráfica para los datos L3/L2 en velocidad")
@@ -102,7 +103,7 @@ plt.grid(color='gray', linestyle='dashed', linewidth=1, alpha=0.4)
 plt.subplot(1,3,3)
 plt.plot(t,f6, color='red', label='Interpolacion Lagrange')
 plt.plot(x1,v3,':x',mew=2, color='royalblue',label='Datos originales')
-plt.legend(loc='upper left')
+plt.legend(loc='upper right')
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("Gráfica para los datos L1/L2 en velocidad")
@@ -116,7 +117,7 @@ L1_1 = L2_1*pglgr3(30)
 L3_1 = L2_1*pglgr2(30)
 
 #Se imprimen los valores hallados por la interpolación, en este caso para los 30° en su criterio de rectitud
-print('\n***RAZONES DE CAMBIO Y VALORES DE LAS DIMENSIONES PARA UN RANGO ANGULAR DE 30° CON RESPECTO A LA RECTITUD***\n')
+print('\n***RAZONES DE CAMBIO Y VALORES DE LAS DIMENSIONES PARA UN RANGO ANGULAR DE 30° CON RESPECTO A LA RECTITUD CON INTERPOLACIÓN DE LAGRANGE***\n')
 print('\ndx/L2(30): ', pglgr1(30))
 print('\nL3/L2(30): ', pglgr2(30))
 print('\nL1/L2(30): ', pglgr3(30))
@@ -130,7 +131,7 @@ L1_2 = L2_2*pglgv3(30)
 L3_2 = L2_2*pglgv2(30)
 
 #Se imprimen los valores hallados por la interpolación, en este caso para los 30° en su criterio de velocidad
-print('\n***RAZONES DE CAMBIO Y VALORES DE LAS DIMENSIONES PARA UN RANGO ANGULAR DE 30° CON RESPECTO A LA VELOCIDAD***\n')
+print('\n***RAZONES DE CAMBIO Y VALORES DE LAS DIMENSIONES PARA UN RANGO ANGULAR DE 30° CON RESPECTO A LA VELOCIDAD CON INTERPOLACIÓN DE LAGRANGE***\n')
 print('\ndx/L2(30): ', pglgv1(30))
 print('\nL3/L2(30): ', pglgv2(30))
 print('\nL1/L2(30): ', pglgv3(30))
@@ -144,7 +145,7 @@ L1_1 = L2_1*pglgr3(55)
 L3_1 = L2_1*pglgr2(55)
 
 #Se imprimen los valores hallados por la interpolación, en este caso para los 55° en su criterio de rectitud
-print('\n***RAZONES DE CAMBIO Y VALORES DE LAS DIMENSIONES PARA UN RANGO ANGULAR DE 55° CON RESPECTO A LA RECTITUD***\n')
+print('\n***RAZONES DE CAMBIO Y VALORES DE LAS DIMENSIONES PARA UN RANGO ANGULAR DE 55° CON RESPECTO A LA RECTITUD CON INTERPOLACIÓN DE LAGRANGE***\n')
 print('\ndx/L2(55): ', pglgr1(55))
 print('\nL3/L2(55): ', pglgr2(55))
 print('\nL1/L2(55): ', pglgr3(55))
@@ -158,7 +159,7 @@ L1_2 = L2_2*pglgv3(55)
 L3_2 = L2_2*pglgv2(55)
 
 #Se imprimen los valores hallados por la interpolación, en este caso para los 55° en su criterio de velocidad
-print('\n***RAZONES DE CAMBIO Y VALORES DE LAS DIMENSIONES PARA UN RANGO ANGULAR DE 55° CON RESPECTO A LA VELOCIDAD***\n')
+print('\n***RAZONES DE CAMBIO Y VALORES DE LAS DIMENSIONES PARA UN RANGO ANGULAR DE 55° CON RESPECTO A LA VELOCIDAD CON INTERPOLACIÓN DE LAGRANGE***\n')
 print('\ndx/L2(55): ', pglgv1(55))
 print('\nL3/L2(55): ', pglgv2(55))
 print('\nL1/L2(55): ', pglgv3(55))
@@ -203,7 +204,7 @@ plt.grid(color='gray', linestyle='dashed', linewidth=1, alpha=0.4)
 plt.subplot(1,3,2)
 plt.plot(t,k2, color='red', label='Interpolacion Splines')
 plt.plot(x1,y2,':x',mew=2, color='royalblue',label='Datos originales')
-plt.legend(loc='upper left')
+plt.legend(loc='upper right')
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("Gráfica para los datos L3/L2 en rectitud")
@@ -212,7 +213,7 @@ plt.grid(color='gray', linestyle='dashed', linewidth=1, alpha=0.4)
 plt.subplot(1,3,3)
 plt.plot(t,k3, color='red', label='Interpolacion Splines')
 plt.plot(x1,y3,':x',mew=2, color='royalblue',label='Datos originales')
-plt.legend(loc='upper left')
+plt.legend(loc='upper right')
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("Gráfica para los datos L1/L2 en rectitud")
@@ -234,7 +235,7 @@ plt.grid(color='gray', linestyle='dashed', linewidth=1, alpha=0.4)
 plt.subplot(1,3,2)
 plt.plot(t,k5, color='red', label='Interpolacion Splines')
 plt.plot(x1,v2,':x',mew=2, color='royalblue',label='Datos originales')
-plt.legend(loc='upper left')
+plt.legend(loc='upper right')
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("Gráfica para los datos L3/L2 en velocidad")
@@ -243,7 +244,7 @@ plt.grid(color='gray', linestyle='dashed', linewidth=1, alpha=0.4)
 plt.subplot(1,3,3)
 plt.plot(t,k6, color='red', label='Interpolacion Splines')
 plt.plot(x1,v3,':x',mew=2, color='royalblue',label='Datos originales')
-plt.legend(loc='upper left')
+plt.legend(loc='upper right')
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title("Gráfica para los datos L1/L2 en velocidad")
@@ -257,7 +258,7 @@ L1_1 = L2_1*pgspr3(30)
 L3_1 = L2_1*pgspr2(30)
 
 #Se imprimen los valores hallados por la interpolación, en este caso para los 30° en su criterio de rectitud
-print('\n***RAZONES DE CAMBIO Y VALORES DE LAS DIMENSIONES PARA UN RANGO ANGULAR DE 30° CON RESPECTO A LA RECTITUD***\n')
+print('\n***RAZONES DE CAMBIO Y VALORES DE LAS DIMENSIONES PARA UN RANGO ANGULAR DE 30° CON RESPECTO A LA RECTITUD CON INTERPOLACIÓN SPLINES***\n')
 print('\ndx/L2(30): ', pgspr1(30))
 print('\nL3/L2(30): ', pgspr2(30))
 print('\nL1/L2(30): ', pgspr3(30))
@@ -271,7 +272,7 @@ L1_2 = L2_2*pgspv3(30)
 L3_2 = L2_2*pgspv2(30)
 
 #Se imprimen los valores hallados por la interpolación, en este caso para los 30° en su criterio de velocidad
-print('\n***RAZONES DE CAMBIO Y VALORES DE LAS DIMENSIONES PARA UN RANGO ANGULAR DE 30° CON RESPECTO A LA VELOCIDAD***\n')
+print('\n***RAZONES DE CAMBIO Y VALORES DE LAS DIMENSIONES PARA UN RANGO ANGULAR DE 30° CON RESPECTO A LA VELOCIDAD CON INTERPOLACIÓN SPLINES***\n')
 print('\ndx/L2(30): ', pgspv1(30))
 print('\nL3/L2(30): ', pgspv2(30))
 print('\nL1/L2(30): ', pgspv3(30))
@@ -285,7 +286,7 @@ L1_1 = L2_1*pgspr3(55)
 L3_1 = L2_1*pgspr2(55)
 
 #Se imprimen los valores hallados por la interpolación, en este caso para los 55° en su criterio de rectitud
-print('\n***RAZONES DE CAMBIO Y VALORES DE LAS DIMENSIONES PARA UN RANGO ANGULAR DE 55° CON RESPECTO A LA RECTITUD***\n')
+print('\n***RAZONES DE CAMBIO Y VALORES DE LAS DIMENSIONES PARA UN RANGO ANGULAR DE 55° CON RESPECTO A LA RECTITUD CON INTERPOLACIÓN SPLINES***\n')
 print('\ndx/L2(55): ', pgspr1(55))
 print('\nL3/L2(55): ', pgspr2(55))
 print('\nL1/L2(55): ', pgspr3(55))
@@ -299,7 +300,7 @@ L1_2 = L2_2*pgspv3(55)
 L3_2 = L2_2*pgspv2(55)
 
 #Se imprimen los valores hallados por la interpolación, en este caso para los 55° en su criterio de velocidad
-print('\n***RAZONES DE CAMBIO Y VALORES DE LAS DIMENSIONES PARA UN RANGO ANGULAR DE 55° CON RESPECTO A LA VELOCIDAD***\n')
+print('\n***RAZONES DE CAMBIO Y VALORES DE LAS DIMENSIONES PARA UN RANGO ANGULAR DE 55° CON RESPECTO A LA VELOCIDAD CON INTERPOLACIÓN SPLINES***\n')
 print('\ndx/L2(55): ', pgspv1(55))
 print('\nL3/L2(55): ', pgspv2(55))
 print('\nL1/L2(55): ', pgspv3(55))
@@ -309,11 +310,11 @@ print('\nL3(55): ', L3_2)
 
 ##CONCLUSIONES
 '''
+Dentro de este código de python se visualizan dos tipos de interpolación, la primera es la interpolación de Lagrange por medio de la librería Scipy
+la segunda es una interpolación de la librería numpy a la cual se le llama Splines, lo que hace esta misma es estimar valores usando un polinomio de grado n
+siendo n el grado del polino por defecto que mejor encuentre la función spline para los datos a los que se le va a hacer la interpolación.
 
-
-
-
-
+Con lo anterior y las gráficas mostradas dentro del código se puede concluir que la función splines muestra un mejor desempeño en esta problematica, ya que 
 
 
 '''
